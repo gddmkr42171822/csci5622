fd1b025f3288bc3f0308e77fe7bd5cbcad05c4aa
gddmkr42171822 <rwghost98@gmail.com>
2016-09-21 11:48:10 -0600

    done with cross validation function.  need to create graphs to show different c's and kernels.

--- csci5622/homework/svm/sample_driver.py
+++ csci5622/homework/svm/sample_driver.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np 
 
 from sklearn.svm import SVC
+from sklearn import cross_validation
 
 class ThreesAndEights:
   """
@@ -34,8 +35,33 @@ class ThreesAndEights:
     self.y_test  = test_set[1][np.where(np.logical_or( test_set[1]==3, test_set[1] == 8))[0]]
     
     f.close()
+    
+  def cross_validate(self, C=None, kernel=None):
+    # Initialize the support vector machine classifier
+    svm = None
+    if C and kernel:
+      svm = SVC(C=C, kernel=kernel)
+    else:
+      svm = SVC()
+    
+    # Combines all of the samples and labels from the training, test, 
+    # and validation sets
+    self.samples = self.x_train
+    self.labels = self.y_train
+    
+    self.samples = np.vstack((self.samples, self.x_test))
+    self.samples = np.vstack((self.samples, self.x_valid))
+    
+    self.labels = np.append(self.labels, self.y_test)
+    self.labels = np.append(self.labels, self.y_valid)
 
-  def predict(self, C=None, kernel=None):
+    # Do cross validation and look at the mean of the scores of the each
+    # of the data splits as the accuracy
+    scores = cross_validation.cross_val_score(
+      svm, self.samples, self.labels, cv=5, n_jobs=-1)
+    print scores.mean()
+
+  def predict(self, C=None, kernel=None, sample=0):
     svm = None
     if C and kernel:
       svm = SVC(C=C, kernel=kernel)
@@ -44,8 +70,7 @@ class ThreesAndEights:
     
     svm.fit(self.x_train, self.y_train)
     
-    print svm.predict(self.x_test[0,:])
-    print svm.score(self.x_test[0,:], self.y_test[0])
+    print 'Prediction: %d\n' % svm.predict(self.x_valid[sample].reshape(1,-1))
 
 def mnist_digit_show(flatimage, outname=None):
 
@@ -69,11 +94,13 @@ if __name__ == "__main__":
   args = parser.parse_args()
   
   data = ThreesAndEights("../data/mnist.pkl.gz")
+
   
   """
   data.x_test is a matrix where one row and all of its columns 
   represent a number like 3 or 8.
     example: data.x_test[0,:] is the first row and all of its columns
+             - when this data is drawn it produces the number 3
   
   data.y_test is an array in which each index is a label for the row of 
   data.x_test
@@ -81,18 +108,16 @@ if __name__ == "__main__":
              data.y_test[1] is the label for data.x_test[1,:]
   """
   
-  
-  print data.x_test[0,:]
-  print data.x_test[1,:]
-  
-  data.predict()
+#   data.cross_validate()
+
   # -----------------------------------
   # Plotting Examples 
   # -----------------------------------
   
   # Display in on screen
-  mnist_digit_show(data.x_test[0,:])  
-  # 	mnist_digit_show(data.x_train[ 0,:])
+#   sample = 0
+#   data.predict(sample=sample)
+#   mnist_digit_show(data.x_valid[sample])  
   
   # Plot image to file 
   # 	mnist_digit_show(data.x_train[1,:], "mnistfig.png")
